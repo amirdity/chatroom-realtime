2a19648817093ed66158dfcd06daec5e0c422cf4
haj amir <amirfattahi22@gmail.com>
2024-09-06 21:52:33 +0330

    add user

---
+++ src/components/list/chatList/addUser/AddUser.tsx
@@ -0,0 +1,20 @@
+import "./addUser.css";
+export default function AddUser() {
+  return (
+    <div className="addUser">
+      <form onSubmit={handleSearch}>
+        <input type="text" placeholder="Username" name="username" />
+        <button>Search</button>
+      </form>
+      {user && (
+        <div className="user">
+          <div className="detail">
+            <img src={user.avatar || "./avatar.png"} alt="" />
+            <span>{user.username}</span>
+          </div>
+          <button onClick={handleAdd}>Add User</button>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,24 +1,29 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import "./login.css";
+import { toast } from "react-toastify";
 export default function Login() {
   const [avatar, setAvatar] = useState({
     file: {},
     url: "",
   });
   console.log(avatar);
+  function handleLogin(e: FormEvent) {
+    e.preventDefault();
+    toast.warn("hello");
+  }
   function handleAvatar(e: React.ChangeEvent<HTMLInputElement>) {
-     if (e.target.files && e.target.files[0]) {
-       setAvatar({
-         file: e.target.files[0],
-         url: URL.createObjectURL(e.target.files[0]),
-       });
-     }
+    if (e.target.files && e.target.files[0]) {
+      setAvatar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
   }
   return (
     <div className="login">
       <div className="item">
         <h2>welcome back</h2>
-        <form action="">
+        <form action="" onSubmit={handleLogin}>
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
           <button>Sign In</button>
@@ -34,7 +39,6 @@ export default function Login() {
           </label>
           <input
             type="file"
-            
             id="file"
             style={{ display: "none" }}
             onChange={handleAvatar}
